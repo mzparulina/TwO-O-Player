8350ca04375b8aeb167aab8bc0981505db6a40dd
mzparulina <pauline.revilla@gmail.com>
2023-05-08 16:47:05 -0400

    Ouput player questions

--- game.rb
+++ game.rb
@@ -5,25 +5,21 @@ class Game
   
   def start
     # Create player 1
-    p1 = Player.new("Player 1")
+    p1 = Players.new("Player 1")
     # Create player 2
-    p2 = Player.new("Player 2")
+    p2 = Players.new("Player 2")
     
-    current_player = rand(1,2)
+    current_player = rand(1..2)
 
     puts "--- Game Start ---"
     puts "--- Choosing Player ---"
-    puts "--- player #{@current_player} selected"
+    puts "--- PLAYER #{current_player} Selected"
 
      ##----------- state of while loop ----------------
     while (p1.lives && p2.lives) > 0 do
       @question = Questions.new
 
-      if current_player % 2 == 0
-        puts "Player 2: #{@question.create_question}"
-      else
-        puts "Player 1: #{@question.create_question}"
-      end
+      puts "Player #{current_player}: #{@question.create_question}"
       answer = gets.chomp
 
     end
