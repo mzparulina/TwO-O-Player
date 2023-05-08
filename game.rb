ff8f79127be58e141de2d004510bf2b2ae9adfe5
mzparulina <pauline.revilla@gmail.com>
2023-05-08 16:26:01 -0400

    Add start game

---
+++ game.rb
@@ -0,0 +1,15 @@
+require './players'
+require './questions'
+
+class Game
+  
+  def start
+    # Create player 1
+    p1 = Player.new("Player 1")
+    # Create player 2
+    p2 = Player.new("Player 2")
+    
+    
+  end
+  
+end
